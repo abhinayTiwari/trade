59bb522273505a8d7cac3a8b5d5d29a68603080f
Abhinay Tiwari <abhinay.indra.tiwari@gmail.com>
2020-09-05 18:46:25 -0700

    added time format

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -3,7 +3,7 @@ import React from 'react'
 function TableRow(props) {
     return (
         <tr>
-            <td>{props.data.timestamp}</td>
+            <td>{updateDateFormat(props.data.timestamp)}</td>
             <td>{props.data.oi_ce}</td>
             <td>{props.data.chng_oi_ce}</td>
             <td>{props.data.oi_pe}</td>
@@ -12,6 +12,10 @@ function TableRow(props) {
             <td>{props.data.vol_pe}</td>
         </tr>
     )
+}
+
+function updateDateFormat(dateSecond){
+    return String(new Date(dateSecond)).substring(0, 28)
 }
 
 export default TableRow
