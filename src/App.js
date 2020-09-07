86edade658d9a05dcb3902b008ea448529c3fa42
Abhinay Tiwari <abhinay.indra.tiwari@gmail.com>
2020-09-07 11:00:04 -0700

    Adding Footer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../src/css/app.css'
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import Container from 'react-bootstrap/Container'
 import TradeTable from './components/TradeTable'
-import '../src/css/app.css';
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <Container className="p-3">
-      <Jumbotron>
-         <TradeTable></TradeTable>
-      </Jumbotron>
-    </Container>
+    <React.Fragment>
+      <Container className="p-3">
+        <Jumbotron>
+          <TradeTable></TradeTable>
+        </Jumbotron>
+      </Container>
+      <Footer />
+    </React.Fragment>
   );
 }
 
