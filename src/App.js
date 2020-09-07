3293d7d437209dc00118cd95e10ff33c9ea95462
Abhinay Tiwari <abhinay.indra.tiwari@gmail.com>
2020-09-07 10:53:14 -0700

    Adding App css to add background image

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 import Container from 'react-bootstrap/Container';
 import TradeTable from './components/TradeTable'
+import '../src/css/app.css';
 
 function App() {
   return (
